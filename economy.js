0a2e0a093c96748a9153885710b6f7c4c2640cb1
Tominous <18189490+Tominous@users.noreply.github.com>
2021-11-13 15:37:26 +0000

    Update economy.js

--- economy.js
+++ economy.js
@@ -25,7 +25,7 @@ client.on('message', message => {
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`)
-    //client.channels.get('518473049197117445').send(`Logged in as ${client.user.tag}`)
+    client.channels.get('518473049197117445').send(`Logged in as ${client.user.tag}`)
 })
 
 
